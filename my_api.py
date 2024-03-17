bddd79d146b9f1f3b588fe1672e210cccf168fec
moyudaduizhang <113818551+moyudaduizhang@users.noreply.github.com>
2024-03-17 09:35:22 +0800

    Update my_api.py

--- my_api.py
+++ my_api.py
@@ -1,206 +1,156 @@
-import dlib
-import numpy as np
-import cv2
-import pymysql
-import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from sshtunnel import SSHTunnelForwarder
-from fastapi import HTTPException
-from pydantic import BaseModel
+from datetime import datetime
+
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from sqlalchemy import Column, String, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
 from PIL import Image
-import pytesseract
 from io import BytesIO
-import io
+import pickle
+from typing import Dict
+import numpy as np
+import uvicorn
+from fastapi.responses import JSONResponse
 from paddleocr import PaddleOCR
+import dlib
+import mysql.connector
 
-# 通过SSH连接云服务器
-server = SSHTunnelForwarder(
-    ssh_address_or_host=("39.101.64.176", 22),  # 云服务器地址IP和端口port
-    ssh_username="root",  # 云服务器登录账号admin
-    ssh_password="20041118Xy?",  # 云服务器登录密码password
-    remote_bind_address=('localhost', 3306)  # 数据库服务地址ip,一般为localhost和端口port，一般为3306
-)
-# 云服务器开启
-server.start()
-# 初始化 Dlib 检测器和模型
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
-face_reco_model = dlib.face_recognition_model_v1(
-    "data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
-
-
-class User(BaseModel):
-    student_id: str
-    file: UploadFile = File(...)
-
-
-# 初始化 FastAPI 应用
 app = FastAPI()
 
+# 修改数据库连接 URL，使用 mysql-connector-python 作为 MySQL 驱动程序
+DATABASE_URL = "mysql+mysqlconnector://yuxialuozi:Lycdemima1@@localhost/budstudent"
+Base = declarative_base()
 
-def decoding_FaceStr(encoding_str):
-    # print("name=%s,encoding=%s" % (name, encoding))
-    # 将字符串转为numpy ndarray类型，即矩阵
-    # 转换成一个list
-    dlist = encoding_str.strip(' ').split(',')
-    # 将list中str转换为float
-    dfloat = list(map(float, dlist))
-    face_encoding = np.array(dfloat)
+
+class FaceEncoding(Base):
+    __tablename__ = "face_recognition"
+
+    id = Column(String(50), primary_key=True, index=True)
+    face_encoding = Column(LargeBinary)
+    student_id = Column(String(50), index=True)
+
+
+# 创建数据库表
+engine = mysql.connector.connect(host="localhost", user="yuxialuozi", password="Lycdemima1@", database="budstudent")
+cursor = engine.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS face_recognition (
+        id VARCHAR(50) PRIMARY KEY,
+        face_encoding LONGBLOB,
+        student_id VARCHAR(50)
+    )
+""")
+engine.commit()
+cursor.close()
+engine.close()
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+face_reco_model = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
+
+
+def compute_face_encoding(image):
+    img = np.array(Image.open(image))
+    dets = detector(img, 1)
+
+    if len(dets) == 0:
+        raise HTTPException(status_code=400, detail="图像中未检测到人脸")
+
+    shape = predictor(img, dets[0])
+    face_encoding = np.array(face_reco_model.compute_face_descriptor(img, shape))
     return face_encoding
 
 
-def encoding_FaceStr(image_face_encoding):
-    # 将numpy array类型转化为列表
-    encoding__array_list = image_face_encoding.tolist()
-    # 将列表里的元素转化为字符串
-    encoding_str_list = [str(i) for i in encoding__array_list]
-    # 拼接列表里的字符串
-    encoding_str = ','.join(encoding_str_list)
-    return encoding_str
+@app.post("/register-face/")
+async def register_face(student_id: int = Form(...), image: UploadFile = File(...)):
+    try:
+        face_encoding = compute_face_encoding(BytesIO(await image.read()))
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn = mysql.connector.connect(host="localhost", user="yuxialuozi", password="Lycdemima1@",
+                                       database="budstudent")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO face_recognition (face_encoding, student_id,create_at,update_at) VALUES (%s, %s, "
+                       "%s,%s)", (pickle.dumps(face_encoding), student_id, current_time, update_time))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return JSONResponse(content={"message": "人脸注册成功"}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {
-        "message": "欢迎使用新苗人脸api（测试中）",
-        "api_version": "1.0",
-        "endpoints": [
-            {"name": "register-face", "description": "人脸注册"},
-            {"name": "recognize-face", "description": "人脸识别"}
-        ],
+@app.post("/recognize-face/")
+async def recognize_face(image: UploadFile = File(...)):
+    try:
+        face_encoding = compute_face_encoding(BytesIO(await image.read()))
+        conn = mysql.connector.connect(host="localhost", user="yuxialuozi", password="Lycdemima1@",
+                                       database="budstudent")
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, face_encoding, student_id FROM face_recognition")
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        for item in result:
+            stored_face_encoding = pickle.loads(item[1])
+            distance = np.linalg.norm(face_encoding - stored_face_encoding)
+            if distance < 0.5:
+                return JSONResponse(content={"student_id": item[2]}, status_code=200)
+
+        return JSONResponse(content={"message": "未找到匹配项"}, status_code=404)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def process_recognized_text(recognized_text: list) -> Dict[str, str]:
+    processed_text = {
+        "学院": "",
+        "专业": "",
+        "学制": "",
+        "籍贯": "",
+        "出生年月": "",
+        "学号": "",
+        "姓名": "",
+        "性别": "",
+        "入学年月": ""
     }
 
+    for line in recognized_text:
+        if "学院：" in line:
+            processed_text["学院"] = line.replace("学院：", "")
+        elif "专业：" in line:
+            processed_text["专业"] = line.replace("专业：", "")
+        elif "班" in line:
+            processed_text["专业"] += line
+        elif "学制：" in line:
+            processed_text["学制"] = line.replace("学制：", "")
+        elif "籍贯：" in line:
+            processed_text["籍贯"] = line.replace("籍贯：", "")
+        elif "出生年月：" in line:
+            processed_text["出生年月"] = line.replace("出生年月：", "")
+        elif "学号：" in line:
+            processed_text["学号"] = line.replace("学号：", "")
+        elif "姓名：" in line:
+            processed_text["姓名"] = line.replace("姓名：", "")
+        elif "性别：" in line:
+            processed_text["性别"] = line.replace("性别：", "")
+        elif "入学年月：" in line:
+            processed_text["入学年月"] = line.replace("入学年月：", "")
 
-# 人脸录入接口
-@app.post("/register-face/")
-async def register_face(student_id: str, file: UploadFile = File(...)):
-    """
-        Register a face for a student.
-
-        Args:
-
-            student_id (str): The ID of the student.
-            file (UploadFile): The image file containing the face to be registered.
-
-        Raises:
-
-            HTTPException: If no face is detected in the uploaded image.
-        """
-    # 函数体
-
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # 使用 Dlib 检测人脸
-    dets = detector(img, 1)
-    if len(dets) == 0:
-        raise HTTPException(status_code=400, detail="No face detected")
-
-    # 取第一个检测到的人脸
-    shape = predictor(img, dets[0])
-
-    face_descriptor = face_reco_model.compute_face_descriptor(img, shape)
-    face_descriptor = np.array(face_descriptor)
-    face_str = encoding_FaceStr(face_descriptor)
-    # 连接到数据库
-    conn = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
-                           port=server.local_bind_port,
-                           user="root",  # mysql的登录账号admin
-                           password="20041118Xy?",  # mysql的登录密码pwd
-                           db="faces",  # mysql中要访问的数据表
-                           charset='utf8')  # 表的字符集
-    cursor = conn.cursor()
-
-    # 将人脸特征和学生学号存入数据库
-
-    cursor.execute('INSERT INTO face_data (face_encoding, student_id) VALUES (%s, %s)',
-                   (face_str, student_id))
-    conn.commit()
-
-    # 关闭数据库连接
-    cursor.close()
-    conn.close()
-
-    return JSONResponse(content={"result": "Face registered"})
+    return processed_text
 
 
-# 人脸识别接口
-@app.post("/recognize-face/")
-async def recognize_face(file: UploadFile = File(...)):
-    """
-        Recognizer a face for a student.
-
-        Args:
-
-            file (UploadFile): The image file containing the face to be recognized.
-
-        Raises:
-
-            HTTPException: If no face is detected in the uploaded image.
-        """
-    # 读取上传的图像文件并进行人脸检测
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    dets = detector(img, 1)
-    if len(dets) == 0:
-        raise HTTPException(status_code=400, detail="No face detected")
-
-    # 取第一个检测到的人脸并计算特征描述子
-    shape = predictor(img, dets[0])
-    face_descriptor = face_reco_model.compute_face_descriptor(img, shape)
-    face_descriptor = np.array(face_descriptor)
-
-    # 连接到数据库
-    conn = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
-                           port=server.local_bind_port,
-                           user="root",  # mysql的登录账号admin
-                           password="20041118Xy?",  # mysql的登录密码pwd
-                           db="faces",  # mysql中要访问的数据表
-                           charset='utf8')  # 表的字符集
-    cursor = conn.cursor()
-
-    # 从数据库中获取存储的人脸特征和学号
-    cursor.execute('SELECT face_encoding, student_id FROM face_data')
-    rows = cursor.fetchall()
-
-    import pickle
-
-    # 遍历数据库中的人脸特征，计算与上传图像的特征的相似度
-    for row in rows:
-        stored_face_descriptor = row[0]
-        face_encoding = decoding_FaceStr(stored_face_descriptor)
-        similarity = np.linalg.norm(face_encoding - face_descriptor)
-
-        # 如果相似度低于阈值，识别为已注册的人脸
-        if similarity < 0.5:
-            student_id = row[1]
-            return JSONResponse(content={"result": f"Face recognized as student {student_id}"})
-
-    # 关闭数据库连接
-    cursor.close()
-    conn.close()
-
-    return JSONResponse(content={"result": "Face not recognized"})
-
-
-@app.post("/paddle_ocr")
+@app.post("/paddle_ocr", tags=["文字识别"])
 async def paddle_ocr(image: UploadFile = File(...)):
-    # 读取上传的图片文件
-    contents = await image.read()
-
-    # 使用 PaddleOCR 进行图片识别
-    ocr = PaddleOCR(use_angle_cls=True, lang="ch")
-    result = ocr.ocr(contents, cls=True)
-
-    # 整理识别结果
-    recognized_text = []
-    for line in result[0]:
-        recognized_text.append(line[1][0])
-
-    return {"recognized_text": recognized_text}
+    try:
+        contents = await image.read()
+        ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+        result = ocr.ocr(contents, cls=True)
+        recognized_text = [line[1][0] for line in result[0]]
+        processed_text = await process_recognized_text(recognized_text)
+        return JSONResponse(content={"recognized_text": processed_text})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="OCR 处理失败")
 
 
 if __name__ == '__main__':
